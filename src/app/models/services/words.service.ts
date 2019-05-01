a2e34ab58d944a68e11563049fa910b993098d70
malkiegoldberger <malkieg970@gmail.com>
2019-05-01 01:00:26 -0400

    getting the words works, just need to make everything pretty, and when login should take you to one of the other pages, if not logged in then cannot visit other pages

--- src/app/models/services/words.service.ts
+++ src/app/models/services/words.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Heroku} from '../heroku';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordsService {
 
-  constructor() { }
+  private proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  private baseUrl = 'https://uncle-pineapple.herokuapp.com/';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      Token: localStorage.getItem('token')
+    })
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getWords(): Observable <Heroku> {
+    return this.http.get<Heroku>(this.proxyUrl + this.baseUrl + 'word', this.httpOptions);
+  }
 }
--- src/app/views/words/words.component.ts
+++ src/app/views/words/words.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WordsService} from '../../models/services/words.service';
 
 @Component({
   selector: 'app-words',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./words.component.scss']
 })
 export class WordsComponent implements OnInit {
-
-  constructor() { }
+words: any;
+  constructor(private wordsService: WordsService) { }
 
   ngOnInit() {
+    this.getWords();
+  }
+
+  getWords(): void {
+    this.wordsService.getWords().subscribe(
+      (res) => {
+        this.words = res;
+      }
+    );
   }
 
 }
